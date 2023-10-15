710c5c0632a1a6dc9c183eed9c79671d8eab797f
Starry <gengweimaster@outlook.com>
2023-10-15 20:57:50 +0800

    rescue code
    thanks god code is back

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const fontFileInput = document.getElementById('fontFile');
-    const textInput = document.getElementById('textInput');
-    const textOutput = document.getElementById('textOutput');
-    const fontOutput = document.getElementById('fontOutput');
+    // 获取页面元素
+    const fontFileInputs = document.querySelectorAll('.font-file-input');
+    const fixedTexts = document.querySelectorAll('.fixed-text');
 
-    let selectedFont = 'sans-serif'; // 默认字体
+    // 遍历所有字体测试模块
+    fontFileInputs.forEach((fontFileInput, index) => {
+        // 默认字体
+        let selectedFont = 'sans-serif';
 
-    // Handle font file input change
-    fontFileInput.addEventListener('change', () => {
-        const file = fontFileInput.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = function () {
-                const fontUrl = URL.createObjectURL(file);
-                const fontName = 'customFont'; // 使用固定名称
-                const font = new FontFace(fontName, `url(${fontUrl})`);
-                font.load().then(() => {
-                    document.fonts.add(font);
-                    selectedFont = fontName;
-                    updateTextFont();
-                    updateFontOutput(file.name.replace(/\.[^.]+$/, '')); // 更新为所选字体文件的文件名（去掉后缀）
-                });
-            };
-            reader.readAsArrayBuffer(file);
+        // 处理字体文件选择
+        fontFileInput.addEventListener('change', () => {
+            const file = fontFileInput.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function () {
+                    const fontUrl = URL.createObjectURL(file);
+                    const fontName = `customFont${index}`; // 使用固定名称
+                    const font = new FontFace(fontName, `url(${fontUrl})`);
+                    font.load().then(() => {
+                        document.fonts.add(font);
+                        selectedFont = fontName;
+                        // 更新文本字体样式
+                        updateTextFont(index, selectedFont);
+                    });
+                };
+                reader.readAsArrayBuffer(file);
+            }
+        });
+
+        // 更新文本字体样式
+        function updateTextFont(textIndex, font) {
+            fixedTexts[textIndex].style.fontFamily = `${font}, sans-serif`;
         }
-    });
-
-    // Update the text font based on the selected font
-    function updateTextFont() {
-        textOutput.style.fontFamily = `${selectedFont}, sans-serif`;
-    }
-
-    // Update the displayed font name (without file extension)
-    function updateFontOutput(fontName) {
-        fontOutput.innerText = `字体: ${fontName}`;
-    }
-
-    // Handle text input change
-    textInput.addEventListener('input', () => {
-        textOutput.innerText = textInput.value;
     });
 });
